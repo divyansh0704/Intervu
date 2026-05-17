baa462be0d9a1c1d7a213eebc81dd169c1a71c33
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2026-05-17 14:44:43 +0530

    cors origin changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://intervu-az8j.onrender.com/api",
+    origin:["http://localhost:5173", "https://intervu-brown.vercel.app/"],
     credentials:true
 }))
 
