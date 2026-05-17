b414ffa1c062a4f6970ec5365474496ab8537ec6
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2026-05-17 14:12:19 +0530

    backend port changes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ const connectDB = require("./src/config/db")
 
 connectDB()
 // generateInterviewReport({resume,jobdescription,selfdescription})
+const PORT = process.env.PORT || 3000
 
-
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
